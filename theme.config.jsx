7d51afc6f3fb58ac9260116114cf684523ce61e8
thame <thame@stakersunion.com>
2024-04-16 07:27:34 -0700

    Clean up default content

---
+++ theme.config.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const config = {
+  logo: <span>Stakers Union Docs</span>,
+  project: {
+    link: 'https://github.com/stakersunion/docs',
+  },
+  chat: {
+    link: 'https://discord.gg/57EJbMGX',
+  },
+  docsRepositoryBase: 'https://github.com/stakersunion/docs',
+  footer: {
+    text: 'Stakers Union',
+  },
+}
+
+export default config
