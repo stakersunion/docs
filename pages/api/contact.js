5527360d486045e5afca00ebb36a6a798fdf784a
thame <thame@stakersunion.com>
2024-04-21 13:00:23 -0700

    Create contact API route
    Using nodemailer

---
+++ pages/api/contact.js
@@ -0,0 +1,36 @@
+export default function handler(req, res) {
+  if (req.method === 'POST') {
+    const { email, message } = req.body
+
+    const { createTransport } = require('nodemailer')
+
+    const transporter = createTransport({
+      host: 'smtp.dreamhost.com',
+      port: 465,
+      secure: true,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    })
+
+    const mailOptions = {
+      from: email,
+      to: 'thame@stakersunion.com',
+      subject: 'Stakers Union Contact Form',
+      text: message,
+    }
+
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log(error)
+        res.status(500).json({ message: 'Error sending email' })
+      } else {
+        console.log('Email sent: ' + info.response)
+        res.status(200).json({ message: 'Email sent successfully' })
+      }
+    })
+  } else {
+    res.status(405).json({ message: 'Method Not Allowed' })
+  }
+}
