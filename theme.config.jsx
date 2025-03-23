f6338afc677c9918d8f75707975d9f85cb339420
thame <tomfadial@gmail.com>
2025-03-22 18:32:11 -0700

    Remove quorum requirement

--- theme.config.jsx
+++ theme.config.jsx
@@ -71,6 +71,18 @@ const config = {
   footer: {
     text: 'Stakers Union',
   },
+  banner: {
+    key: 'no-quorum',
+    text: (
+      <a
+        href={'https://github.com/stakersunion/docs/compare/main...no-quorum'}
+        target={'_blank'}
+      >
+        📜 This is a proposal for <strong>'no-quorum'</strong> | View changes →
+      </a>
+    ),
+    dismissible: false,
+  },
 }
 
 export default config
